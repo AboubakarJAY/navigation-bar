9a0e7f73f8c5333a6d398c2b22b991094bcf9600
AboubakarJay <aboubakarjay2@hotmail.com>
2025-02-12 00:29:14 +0100

    12 feb.

--- src/app.js
+++ src/app.js
@@ -15,7 +15,20 @@ for (let i = 0; i < element.length; i++) {
     element[i].addEventListener("mousemove", (e) => {
         followMouse.style.display = "block";
         let mouseposition = e;
-        followMouse.style.left = `${mouseposition.clientX + 50}px`;
-        followMouse.style.top = `${mouseposition.clientY - 50}px`;
+        let target = e.target;
+        let parent = target.parentElement;
+        if (parent && target === parent.firstElementChild) {
+            followMouse.style.left = `${mouseposition.clientX}px`;
+            followMouse.style.top = `${mouseposition.clientY - 60}px`;
+        }
+        else {
+            followMouse.style.left = `${mouseposition.clientX + 50}px`;
+            followMouse.style.top = `${mouseposition.clientY - 50}px`;
+        }
+    });
+}
+for (let i = 0; i < element.length; i++) {
+    element[i].addEventListener("mouseleave", () => {
+        followMouse.style.display = "none";
     });
 }
